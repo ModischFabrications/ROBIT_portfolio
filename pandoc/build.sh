bd5c1b923fd1535fb70d71e8726dfbd18a138fa4
Robin Modisch <robin.modisch@fau.de>
2020-09-01 15:30:59 +0200

    use local template for pandoc build

--- pandoc/build.sh
+++ pandoc/build.sh
@@ -1,2 +1,2 @@
 pandoc *.md title.yaml -s -o book.html;
-pandoc book.html --pdf-engine=xelatex --template=eisvogel.tex --toc --metadata-file=metadata.yaml -o portfolio.pdf
+pandoc book.html --pdf-engine=xelatex --template=./templates/eisvogel.tex --toc --metadata-file=metadata.yaml -o portfolio.pdf
