0c0f0f7231c4e155d52c39bcfef66fbb43c544a3
Robin Modisch <robin.modisch@fau.de>
2020-09-01 15:37:57 +0200

    skip intermediate HTML

--- pandoc/build.sh
+++ pandoc/build.sh
@@ -1,2 +1,1 @@
-pandoc *.md title.yaml -s -o book.html;
-pandoc book.html --pdf-engine=xelatex --template=./templates/eisvogel.tex --toc --metadata-file=metadata.yaml -o portfolio.pdf
+pandoc *.md title.yaml --pdf-engine=xelatex --template=./templates/eisvogel.tex --toc --metadata-file=metadata.yaml -o portfolio.pdf
